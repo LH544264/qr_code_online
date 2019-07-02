7735df9dd804cb8ae40655cf0975fb4b89b9cd62
LH <you@111.com>
2019-07-02 10:33:17 +0800

    优化二维码的图像显示方式，以接口的方式提供生成二维码的功能

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import io
 import flask 
 import qrcode
 from flask import render_template
@@ -17,12 +18,20 @@ def test():
     # return '<img src="/static/qr.jpg"/>' 
     return flask.render_template('hello.html')
     
-@app.route("/qr", methods=["POST"])
+@app.route("/qr")
 def qr():
-    data = flask.request.form.get("data")
+    data = flask.request.args.get("data")
+
     img = qrcode.make(data)
-    img.save(r"C:\Users\Administrator\Desktop\Python\qrcode_tool_online\static\qr.jpg")
-    return '<img src="/static/qr.jpg"/>'
+    bi = io.BytesIO() # 创建一个BytesIO对象，用于在内存中存储二维码图像数据
+    img.save(bi, "png") # 调用img对象的save方法将二维码图像数据以PNG编码格式写入bi对象管理的内存空间
+
+    # img.save(r"C:\Users\Administrator\Desktop\Python\qrcode_tool_online\static\qr.jpg")
+    # return '<img src="/static/qr.jpg"/>'
+    bi.seek(0)  # 将bi对象内部指针移动到图像数据的起始位置
+
+    # 返回二维码图像数据
+    return flask.send_file(bi, "image/png")
 
 if __name__ == '__main__':
     app.run(debug=True)
